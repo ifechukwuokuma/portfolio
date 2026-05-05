f24971d07d1542d2e7b7837f9f50ab0042dc2ab9
ifechukwuokuma <okumaprecious20@gmail.com>
2026-05-05 15:35:10 +0100

    Fix ServicesSection import path/casing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,6 @@
 import { Hero } from "../components/Sections/Hero";
-import { ServicesSection } from "../components/Sections/ServicesSection";
 import { ProjectsSection } from "../components/Sections/ProjectsSection";
 import { TechStackTabs } from "../components/Sections/TechStackTabs";
-import { AboutSection } from "../components/Sections/AboutSection";
-import { ContactSection } from "../components/Sections/ContactSection";
 import { Footer } from "../components/Sections/Footer";
 
 
